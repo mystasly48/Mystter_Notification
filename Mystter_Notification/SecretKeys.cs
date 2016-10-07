1dd0d0dcac4d68334a3e246a450375cac149a213
Maisu <mystasly48@gmail.com>
2016-10-07 18:43:52 +0900

    Added: SecretKeys

---
+++ Mystter_Notification/SecretKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Mystter_Notification {
+    public class SecretKeys {
+        public static readonly string ConsumerKey = "";
+        public static readonly string ConsumerSecret = "";
+
+        public static readonly string AccessToken = "";
+        public static readonly string AccessTokenSecret = "";
+    }
+}
