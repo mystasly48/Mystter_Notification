f81e3ddd9d0f3a65b4a32e26971927c4452fef2c
Maisu <mystasly48@gmail.com>
2016-10-10 02:33:03 +0900

    Added: Receive Followed, Liked, Unliked, Quoted, Listed, Unlisted, Subscribed, Unsubscribed Notify

--- Mystter_Notification/Form1.cs
+++ Mystter_Notification/Form1.cs
@@ -16,6 +16,8 @@ namespace Mystter_Notification {
         bool HideForm = true;
         Tokens twitter;
         string BalloonClickedUrl = "";
+        const string SCREEN_NAME = "30msl";
+        const string TWITTER_URL = "https://twitter.com/";
 
         private void Form1_Load(object sender, EventArgs e) {
             TwitterInit();
@@ -37,12 +39,33 @@ namespace Mystter_Notification {
         }
 
         private void Streaming() {
-            
+            var userStream = twitter.Streaming.UserAsObservable().Publish();
+            userStream.OfType<EventMessage>().Where(m => m.Source.ScreenName != SCREEN_NAME).Subscribe(m => ReceivedEventMessage(m));
+            userStream.Connect();
         }
 
+        private void ReceivedEventMessage(EventMessage m) {
+            var user = $"{m.Source.Name} (@{m.Source.ScreenName})";
+            var userAndEvent = GetEventName(m.Event) + " : " + user;
+            var targetStatusText = (m.TargetStatus != null) ? m.TargetStatus.Text : null;
+            var targetListName = (m.TargetList != null) ? m.TargetList.Name : null;
+            Debug.WriteLine(userAndEvent);
+            if (m.Event == EventCode.Follow) {
+                ShowBalloonTipAsync(userAndEvent);
+                BalloonClickedUrl = TWITTER_URL + m.Source.ScreenName;
+            } else if (m.Event == EventCode.Favorite || m.Event == EventCode.Unfavorite || m.Event == EventCode.QuotedTweet) {
+                ShowBalloonTipAsync(targetStatusText, userAndEvent);
+                BalloonClickedUrl = TWITTER_URL + m.TargetStatus.User.ScreenName + "/status/" + m.TargetStatus.Id;
+            } else if (m.Event == EventCode.ListMemberAdded || m.Event == EventCode.ListMemberRemoved || m.Event == EventCode.ListUserSubscribed || m.Event == EventCode.ListUserUnsubscribed) {
+                ShowBalloonTipAsync(targetListName, userAndEvent);
+                BalloonClickedUrl = TWITTER_URL + m.Source.ScreenName + "/lists/" + m.TargetList.Slug;
+            } else {
+                ShowBalloonTipAsync(m.Event.ToString());
+            }
+        }
 
-        private delegate void ShowBalloonTipCallBack(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None);
-        private void ShowBalloonTipAsync(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None) {
+        private delegate void ShowBalloonTipCallBack(string text = "", string title = "", int timeout = 3000, ToolTipIcon icon = ToolTipIcon.None);
+        private void ShowBalloonTipAsync(string title, string text = "", int timeout = 3000, ToolTipIcon icon = ToolTipIcon.None) {
             if (InvokeRequired) {
                 var method = new ShowBalloonTipCallBack(ShowBalloonTipAsync);
                 Invoke(method, new object[] { text, title, timeout, icon });
@@ -51,7 +74,7 @@ namespace Mystter_Notification {
             }
         }
 
-        private void ShowBalloonTip(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None, string url = "") {
+        private void ShowBalloonTip(string text = "", string title = "", int timeout = 3000, ToolTipIcon icon = ToolTipIcon.None, string url = "") {
             notifyIcon1.BalloonTipText = text;
             notifyIcon1.BalloonTipTitle = title;
             notifyIcon1.BalloonTipIcon = icon;
@@ -66,6 +89,29 @@ namespace Mystter_Notification {
         private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e) {
             if (!string.IsNullOrEmpty(BalloonClickedUrl))
                 Process.Start(BalloonClickedUrl);
+        }
+
+        private string GetEventName(EventCode code) {
+            switch (code) {
+                case EventCode.Follow:
+                    return "Followed";
+                case EventCode.Favorite:
+                    return "Liked";
+                case EventCode.Unfavorite:
+                    return "Unliked";
+                case EventCode.QuotedTweet:
+                    return "Quoted";
+                case EventCode.ListMemberAdded:
+                    return "Listed";
+                case EventCode.ListMemberRemoved:
+                    return "Unlisted";
+                case EventCode.ListUserSubscribed:
+                    return "Subscribed";
+                case EventCode.ListUserUnsubscribed:
+                    return "Unsubscribed";
+                default:
+                    return null;
+            }
         }
     }
 }
