4fa11bde59289542344e8c85ba20c01d80bfea85
Maisu <mystasly48@gmail.com>
2016-10-09 23:51:34 +0900

    Added: ShowBalloonTip call from another thread

--- Mystter_Notification/Form1.cs
+++ Mystter_Notification/Form1.cs
@@ -2,6 +2,10 @@
 using System.Threading;
 using System.Windows.Forms;
 using CoreTweet;
+using CoreTweet.Streaming;
+using System.Reactive.Linq;
+using System.Diagnostics;
+using System.Media;
 
 namespace Mystter_Notification {
     public partial class Form1 : Form {
@@ -11,36 +15,57 @@ namespace Mystter_Notification {
 
         bool HideForm = true;
         Tokens twitter;
+        string BalloonClickedUrl = "";
 
         private void Form1_Load(object sender, EventArgs e) {
+            TwitterInit();
+
             var streamThread = new Thread(new ThreadStart(Streaming));
             streamThread.Start();
 
-            if (HideForm) Opacity = 0;
+            if (HideForm)
+                Opacity = 0;
+        }
+
+        private void TwitterInit() {
+            twitter = Tokens.Create(SecretKeys.ConsumerKey, SecretKeys.ConsumerSecret, SecretKeys.AccessToken, SecretKeys.AccessTokenSecret);
         }
 
         private void Form1_Shown(object sender, EventArgs e) {
-            if (HideForm) Hide();
+            if (HideForm)
+                Hide();
         }
 
         private void Streaming() {
-            var stream = twitter.
+            
         }
 
-        private void ShowBalloonTip(string text, string title, ToolTipIcon icon, int timeout) {
+
+        private delegate void ShowBalloonTipCallBack(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None);
+        private void ShowBalloonTipAsync(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None) {
+            if (InvokeRequired) {
+                var method = new ShowBalloonTipCallBack(ShowBalloonTipAsync);
+                Invoke(method, new object[] { text, title, timeout, icon });
+            } else {
+                ShowBalloonTip(text, title, timeout, icon);
+            }
+        }
+
+        private void ShowBalloonTip(string text, string title, int timeout = 1000, ToolTipIcon icon = ToolTipIcon.None, string url = "") {
             notifyIcon1.BalloonTipText = text;
             notifyIcon1.BalloonTipTitle = title;
             notifyIcon1.BalloonTipIcon = icon;
             notifyIcon1.ShowBalloonTip(timeout);
-        }
-
-        private void button1_Click(object sender, EventArgs e) {
-            ShowBalloonTip("Text", "Title", ToolTipIcon.Warning, 1000);
-
+            BalloonClickedUrl = url;
         }
 
         private void 終了ToolStripMenuItem_Click(object sender, EventArgs e) {
             Application.Exit();
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e) {
+            if (!string.IsNullOrEmpty(BalloonClickedUrl))
+                Process.Start(BalloonClickedUrl);
         }
     }
 }
